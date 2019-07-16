882d63e40f45b1440d4e16c5a9db1ebaba855f9a
julaporn <52956562+julaporn@users.noreply.github.com>
2019-07-16 17:19:28 +0700

    Add files via upload

---
+++ pub.php
@@ -0,0 +1,40 @@
+ <?php
+ function pubMqtt($topic,$msg) {
+    $APPID= "MinkyLineRobot/";      //PUT NETPIE APPID Name end with "/"
+    $KEY = "FTSJqOMU4gDgyHw";          //PUT NETPIE Key ID
+    $SECRET = "FyBK6CJ84QGo8RgJkocpFhg7S";   //PUT NETPIE Secret ID 
+    $Topic = "$topic";
+  
+    put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
+  }
+
+ function getMqttfromlineMsg($Topic,$lineMsg) {
+    $pos = strpos($lineMsg, ":");
+    if($pos) {
+      $splitMsg = explode(":", $lineMsg);
+      $topic = $splitMsg[0];
+      $msg = $splitMsg[1];
+      pubMqtt($topic,$msg);
+    }
+    else {
+      $topic = $Topic;
+      $msg = $lineMsg;
+      pubMqtt($topic,$msg);
+    }
+  }
+ 
+  function put($url,$tmsg) {
+     $ch = curl_init($url);
+     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+     curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
+     
+     $response = curl_exec($ch);
+     curl_close($ch);
+     echo $response . "\r\n";
+    return $response;
+  }
+
+?>
