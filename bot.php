4f7e4329d1518d7979b0ab210ebfc5b722fbb2b8
julaporn <52956562+julaporn@users.noreply.github.com>
2019-07-17 11:35:21 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -9,8 +9,8 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 
 // Validate parsed JSON data
-if (!is_null($events['ESP'])) {
-   send_LINE($events['ESP']);
+if (!is_null($events['NodeMCU_No1'])) {
+   send_LINE($events['NodeMCU_No1']);
    echo "JSON data OK";
 }
 
