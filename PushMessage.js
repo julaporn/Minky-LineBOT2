3abd9ae357817a54d88dbdd8a282905e3689c567
julaporn <52956562+julaporn@users.noreply.github.com>
2019-08-22 19:35:45 +0700

    Add files via upload

--- PushMessage.js
+++ PushMessage.js
@@ -1,29 +1,33 @@
 
-const functions = require("firebase-functions");
-const request = require("request-promise");
+const functions = require('firebase-functions');
+const request = require('request-promise');
 
-const LINE_MESSAGING_API = "https://api.line.me/v2/bot/message";
+const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
 const LINE_HEADER = {
-  "Content-Type": "application/json",
-  "Authorization": "Bearer 4ipMPNdnISFqBZJ+Zdx4cvMn8/L+RaD76H7xDQJElyeWKQsLD2qh86aVmXs+wHu3o0BmRn83h0ibHrMwGAFr3H5WHe5dzmYi4Ik13FjFDCB9RAC4wZPwenW+CItiqydLDaAK05DZreUfsHU42wNBcgdB04t89/1O/w1cDnyilFU=
-"
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer 4ipMPNdnISFqBZJ+Zdx4cvMn8/L+RaD76H7xDQJElyeWKQsLD2qh86aVmXs+wHu3o0BmRn83h0ibHrMwGAFr3H5WHe5dzmYi4Ik13FjFDCB9RAC4wZPwenW+CItiqydLDaAK05DZreUfsHU42wNBcgdB04t89/1O/w1cDnyilFU=`
 };
 
-exports.BasicMessage = functions.https.onRequest((req, res) => {
+exports.LineBot = functions.https.onRequest((req, res) => {
+  if (req.body.events[0].message.type !== 'text') {
+    return;
+  }
+  reply(req.body);
+});
+
+const reply = (bodyResponse) => {
   return request({
-    method: "POST",
-    uri: `${LINE_MESSAGING_API}/push`,
+    method: `POST`,
+    uri: `${LINE_MESSAGING_API}/reply`,
     headers: LINE_HEADER,
     body: JSON.stringify({
-      to: "U930cda3cddf9ba7693afa910d00858eb",
-      messages: [{
-          type: "text",
-          text: "LINE \uDBC0\uDC84 x \uDBC0\uDCA4 Firebase"
-      }]
+      replyToken: bodyResponse.events[0].replyToken,
+      messages: [
+        {
+          type: `text`,
+          text: bodyResponse.events[0].message.text
+        }
+	  ]
     })
-  }).then(() => {
-      return res.status(200).send("Done");
-  }).catch(error => {
-      return Promise.reject(error);
   });
-});
+};
