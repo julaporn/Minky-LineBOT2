a7f244d23fb4a9d18b52fa9c4587da3b466b4cf5
julaporn <52956562+julaporn@users.noreply.github.com>
2019-08-23 13:50:37 +0700

    Add files via upload

--- Node2Line.php
+++ Node2Line.php
@@ -1,6 +1,6 @@
 <?php
    $accessToken = "4ipMPNdnISFqBZJ+Zdx4cvMn8/L+RaD76H7xDQJElyeWKQsLD2qh86aVmXs+wHu3o0BmRn83h0ibHrMwGAFr3H5WHe5dzmYi4Ik13FjFDCB9RAC4wZPwenW+CItiqydLDaAK05DZreUfsHU42wNBcgdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
-   $id = = "U930cda3cddf9ba7693afa910d00858eb";
+   $id = "U930cda3cddf9ba7693afa910d00858eb";
    //****************************
    $content = file_get_contents('php://input');
    $arrayJson = json_decode($content, true);
